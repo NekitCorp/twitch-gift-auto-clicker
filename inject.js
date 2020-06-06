e2a79ca5dae7033d2b4a01e25d1ee82906d202e5
Nikita <madeev@itglobal.ru>
2020-06-06 20:52:20 +0300

    fix change stream

--- inject.js
+++ inject.js
@@ -5,35 +5,37 @@ chrome.extension.sendMessage({}, function (response) {
 
             // ----------------------------------------------------------
             // This part of the script triggers when page is done loading
-            const observer = new MutationObserver(function (mutationsList) {
-                for (const mutation of mutationsList) {
-                    for (const addedNode of mutation.addedNodes) {
-                        if (
-                            addedNode.querySelector &&
-                            addedNode.querySelector(".claimable-bonus__icon")
-                        ) {
-                            // log
-                            console.log(
-                                `I clicked for you ${
-                                    document
-                                        .querySelector(".community-points-summary")
-                                        .querySelector('[data-test-selector="balance-string"]')
-                                        .innerText
-                                } ${new Date().toLocaleTimeString()}`
-                            );
-
-                            // click
-                            addedNode.querySelector("button").click();
-                        }
-                    }
-                }
-            });
-
-            observer.observe(document.querySelector(".community-points-summary"), {
-                childList: true,
-                subtree: true,
-            });
+            startObserver();
             // ----------------------------------------------------------
         }
     }, 10);
 });
+
+function startObserver() {
+    const observer = new MutationObserver(function (mutationsList) {
+        for (const mutation of mutationsList) {
+            for (const addedNode of mutation.addedNodes) {
+                if (addedNode.querySelector && addedNode.querySelector(".claimable-bonus__icon")) {
+                    // log
+                    console.log(
+                        `I clicked for you ${
+                            document
+                                .querySelector(".community-points-summary")
+                                .querySelector('[data-test-selector="balance-string"]').innerText
+                        } ${new Date().toLocaleTimeString()}`
+                    );
+
+                    // click
+                    addedNode.querySelector("button").click();
+                }
+            }
+        }
+    });
+
+    observer.observe(document.body, {
+        subtree: true,
+        childList: true,
+    });
+
+    console.log("I started to follow the gitf o_o");
+}

66fbf1b5e3d6494fa32b313181b101cdcdd3eef5
Nikita Madeev <warhammer-ss@yandex.ru>
2023-09-19 09:03:54 +0300

    feat: :sparkles: Add link to channel name. Sort channels by points. Optimize icons size.

--- popup.js
+++ popup.js
@@ -2,22 +2,36 @@
 
 // @ts-ignore
 const storage = chrome.storage.local;
+// @ts-ignore
+const tabs = chrome.tabs;
 
 storage.get().then((data) => {
+    const sortedData = Object.entries(data).sort((a, b) => b[1] - a[1]);
     const tbody = document.querySelector("tbody");
 
     if (!(tbody instanceof HTMLTableSectionElement)) return;
 
-    for (const key in data) {
+    for (const [channelName, channelPoints] of sortedData) {
         const tr = document.createElement("tr");
         tbody.appendChild(tr);
 
         const tdChannel = document.createElement("td");
-        tdChannel.textContent = key;
+        const aChannel = document.createElement("a");
+        aChannel.href = `https://www.twitch.tv/${channelName}`;
+        aChannel.textContent = channelName;
+        tdChannel.appendChild(aChannel);
         tr.appendChild(tdChannel);
 
         const tdPoints = document.createElement("td");
-        tdPoints.textContent = data[key];
+        tdPoints.textContent = channelPoints;
         tr.appendChild(tdPoints);
     }
+
+    tbody.addEventListener("click", (event) => {
+        const target = event.target;
+
+        if (target instanceof HTMLAnchorElement) {
+            tabs.create({ url: target.getAttribute("href") });
+        }
+    });
 });
